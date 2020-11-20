bd2eeba33f0b63c23a195af186963038a72cdffd
mikikuri <67615080+mikikuri@users.noreply.github.com>
2020-11-20 21:33:34 +1100

    Add files via upload

---
+++ scripts/check_youtube.py
@@ -0,0 +1,58 @@
+﻿#!/usr/bin/env python3
+# coding: utf-8
+
+import re
+import json
+import requests
+import argparse
+from retrying import retry
+
+headers = {
+    'Accept-Language': 'en-US,en;q=0.8',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36',
+}
+
+@retry(stop_max_attempt_number=3)
+def get(url: str) -> str:
+    try:
+        r = requests.get(url, headers=headers)
+        return r.text
+    except requests.RequestException:
+        pass
+        
+# Extract video id from HTML of channel page.
+def get_live_video_info_from_html(html: str) -> dict:
+    regex = r'"channelFeaturedContentRenderer":{"items":\[{"videoRenderer":{"videoId":"(.{11})'
+    return re.search(regex, html).group(1)
+
+def check_channel_live_streaming(channel_id: str) -> bool:
+    try:
+        html = get(f'https://www.youtube.com/channel/{channel_id}/featured')
+        if '"label":"LIVE"' in html:
+            # video_info = get_live_video_info_by_channel_id(channel_id)
+            video_info = get_live_video_info_from_html(html)
+            return video_info
+        else:
+            return False
+    except Exception:
+        raise
+
+# usage: ./check_youtube.py youtube_channel_id
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser(
+        description='A tool to check if a channel currently has a live stream.',
+        formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('cid', help='YouTube Channel ID')
+    args = parser.parse_args()
+    
+    channel_id = args.cid
+
+    info = check_channel_live_streaming(channel_id)
+
+    if info:
+        print ("https://www.youtube.com/watch?v=%s" % (info))
+    else:
+        # print(f'No live streams for channel {channel_id} available now', file=sys.stderr)
+        print ("Not_Live")
+    exit(1)
---
+++ scripts/parse_json.py
@@ -0,0 +1,66 @@
+﻿#!/usr/bin/env python3.9
+
+import json
+import datetime
+import time
+import sys
+import getopt
+
+def main(argv):
+    # parse input arguments
+    inputfile = ''
+    outputfile = ''
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+    except getopt.GetoptError:
+        print ('test.py -i <inputfile> -o <outputfile>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print ('test.py -i <inputfile> -o <outputfile>')
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+    
+    with open(inputfile) as json_file:
+        # import data from input .json file
+        print(json_file)
+        data = json.load(json_file)
+        i = 0
+        
+        # set initial timestamp to be 00:00
+        startTime = data[0]['timestamp']
+        starttime_formatted = datetime.datetime.fromtimestamp(startTime / 1000000)
+        
+        # add time_text, time_in_seconds, video_offset_time_msec fields to each chat entry
+        while i < len(data):
+            
+            # set chat message timestamp
+            timestamp = data[i]['timestamp']
+            # convert timestamp to datetime format - /1000000 as epoch timestamp is in mili/micro seconds
+            timestamp_formatted = datetime.datetime.fromtimestamp(timestamp / 1000000)
+            # calculate the difference between initial and current chat timestamp
+            difference_in_sec = int((timestamp_formatted - starttime_formatted).total_seconds())
+            # calculate the milisecond difference between initial and current chat timestamp
+            video_offset = int((timestamp_formatted - starttime_formatted).total_seconds() * 1000)
+            
+            # if time is less than 1hr use mm:ss, else use hh:mm:ss for time_text
+            if difference_in_sec < 3600:
+                tt_formatted = time.strftime('%M:%S', time.gmtime(difference_in_sec))
+            else:
+                tt_formatted = time.strftime('%H:%M:%S', time.gmtime(difference_in_sec))
+            
+            # add entries to json
+            data[i]['time_text'] = tt_formatted
+            data[i]['time_in_seconds'] = difference_in_sec
+            data[i]['video_offset_time_msec'] = video_offset
+            i+=1
+
+        # write data to output .json file
+        with open(outputfile, 'w') as outfile:
+                json.dump(data, outfile)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
